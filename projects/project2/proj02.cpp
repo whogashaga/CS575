08de820604479f5afbfeb8eabf5cbd27c74f9cf4
YenChun Chen <chenyenc@flip3.engr.oregonstate.edu>
2024-04-19 16:47:04 -0700

    Only MyAgent() not completed

--- projects/project2/proj02.cpp
+++ projects/project2/proj02.cpp
@@ -11,10 +11,10 @@
 int	NowYear = 2024;		// 2024- 2029
 int	NowMonth = 0;		// 0 - 11
 
-float	NowPrecip;		// inches of rain per month
+float NowPrecip;		// inches of rain per month
 float NowTemp;          // fahrenheit temperature this month
-float	NowHeight  = 5.;	// grain height in inches
-int 	NowNumDeer = 2;		// number of deer in the current population
+float NowHeight  = 5.;	// grain height in inches
+int NowNumDeer   = 2;	// number of deer in the current population
 
 const float GRAIN_GROWS_PER_MONTH   = 12.0;
 const float ONE_DEER_EATS_PER_MONTH = 1.0;
@@ -42,56 +42,9 @@ float Ranf(float low, float high)
     return low + t * (high - low);
 }
 
-void Watcher()
+float SQR(float x)
 {
-    while (NowYear < 2030)
-    {
-        float ang = (30. * (float)NowMonth + 15.) * (M_PI / 180.); // angle of earth around the sun
-
-        float temp = AVG_TEMP - AMP_TEMP * cos(ang);
-        NowTemp = (5. / 9.) * (temp + Ranf(-RANDOM_TEMP, RANDOM_TEMP) - 32);
-
-        float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin(ang);
-        NowPrecip = (precip + Ranf(-RANDOM_PRECIP, RANDOM_PRECIP)) * 2.54;
-        if (NowPrecip < 0.)
-            NowPrecip = 0.;
-
-        fprintf(stderr, "%2d, %d - Temp: %5.2lf °C, Precip: %5.2lf cm\n", NowMonth + 1, NowYear, NowTemp, NowPrecip);
-        
-        if (NowMonth < 11) {
-            NowMonth++;
-        } else {
-            NowMonth = 0;
-            if (NowYear < 2030) 
-                NowYear++;
-        }
-    }
-    
-}
-
-void Deer()
-{
-    while (NowYear < 2030)
-    {
-        /* code */
-    }
-    
-}
-
-void Grain()
-{
-    while (NowYear < 2030)
-    {
-        /* code */
-    }
-}
-
-void MyAgent()
-{
-    while (NowYear < 2030)
-    {
-        /* code */
-    }
+    return x * x;
 }
 
 // specify how many threads will be in the barrier:
@@ -115,7 +68,8 @@ void WaitBarrier()
             // let all other threads get back to what they were doing
             // before this one unlocks, knowing that they might immediately
             // call WaitBarrier( ) again:
-            while (NumGone != NumInThreadTeam - 1);
+            while (NumGone != NumInThreadTeam - 1)
+                ;
             omp_unset_lock(&Lock);
             return;
         }
@@ -125,8 +79,114 @@ void WaitBarrier()
     while (NumAtBarrier != 0)
         ; // this waits for the nth thread to arrive
 
-    #pragma omp atomic
-        NumGone++; // this flags how many threads have returned
+#pragma omp atomic
+    NumGone++; // this flags how many threads have returned
+}
+
+void Watcher()
+{
+    while (NowYear < 2030)
+    {
+        // DoneComputing barrier:
+        WaitBarrier();
+
+        // DoneAssigning barrier:
+        WaitBarrier();
+
+        float ang = (30. * (float)NowMonth + 15.) * (M_PI / 180.); // angle of earth around the sun
+
+        float temp = AVG_TEMP - AMP_TEMP * cos(ang);
+        NowTemp = temp + Ranf(-RANDOM_TEMP, RANDOM_TEMP);
+        float centigrade = (5. / 9.) * (NowTemp - 32);
+
+        float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin(ang);
+        NowPrecip = precip + Ranf(-RANDOM_PRECIP, RANDOM_PRECIP);
+        if (NowPrecip < 0.)
+            NowPrecip = 0.;
+        float precipCm = (NowPrecip < 0.) ? 0. : (NowPrecip * 2.54);
+
+        fprintf(stderr, "%2d, %d - Temp: %5.2lf °C, Precip: %5.2lf cm | Grain: %6.2lf cm, Deers: %2d\n",
+                NowMonth, NowYear, centigrade, precipCm, NowHeight*2.54, NowNumDeer);
+
+        if (NowMonth < 11) {
+            NowMonth++;
+        }
+        else {
+            NowMonth = 0;
+            if (NowYear < 2030)
+                NowYear++;
+        }
+        // DonePrinting barrier:
+        WaitBarrier();
+    }
+}
+
+void Deer()
+{
+    while (NowYear < 2030)
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+        int nextNumDeer = NowNumDeer;
+        int carryingCapacity = (int)(NowHeight);
+        if (nextNumDeer < carryingCapacity)
+            nextNumDeer++;
+        else if (nextNumDeer > carryingCapacity)
+            nextNumDeer--;
+
+        // DoneComputing barrier:
+        WaitBarrier();
+
+        NowNumDeer = (nextNumDeer < 0) ? 0 : nextNumDeer;
+        // DoneAssigning barrier:
+        WaitBarrier();
+
+        // DonePrinting barrier:
+        WaitBarrier();
+    }
+}
+
+void Grain()
+{
+    while (NowYear < 2030)
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+        float tempFactor = exp(-SQR((NowTemp - MIDTEMP) / 10.));
+        float precipFactor = exp(-SQR((NowPrecip - MIDPRECIP) / 10.));
+
+        float nextHeight = NowHeight;
+        nextHeight += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
+        nextHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
+
+        // DoneComputing barrier:
+        WaitBarrier();
+
+        NowHeight = (nextHeight < 0.) ? 0. : nextHeight;
+        // DoneAssigning barrier:
+        WaitBarrier();
+
+        // DonePrinting barrier:
+        WaitBarrier();
+    }
+}
+
+void MyAgent()
+{
+    while (NowYear < 2030)
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+
+        // DoneComputing barrier:
+        WaitBarrier();
+
+        // DoneAssigning barrier:
+        WaitBarrier();
+
+        // DonePrinting barrier:
+        WaitBarrier();
+    }
 }
 
 int main(int argc, char *argv[])
