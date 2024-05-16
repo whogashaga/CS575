595d5b204173cc36381ecc7ed1aaf313d745106f
YenChun Chen <chenyenc@rabbit.engr.oregonstate.edu>
2024-05-16 15:17:16 -0700

    create sample code arrayMul

---
+++ projects/project5/arrayMul.cu
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+#include <math.h>
+#include <stdlib.h>
+// CUDA runtime
+#include <cuda_runtime.h>
+// Helper functions and utilities to work with CUDA
+#include "helper_functions.h"
+#include "helper_cuda.h"
+#ifndef THREADS_PER_BLOCK
+#define THREADS_PER_BLOCK 128 // number of threads in each block
+#endif
+#ifndef DATASET_SIZE // WARNING: DON’T CALL THIS “ARRAYSIZE” !#define DATASET_SIZE ( 8*1024*1024 ) // size of the array
+#endif
+float hA[ DATASET_SIZE ];
+float hB[ DATASET_SIZE ];
+float hC[ DATASET_SIZE ];
+
+void
+CudaCheckError( )
+{
+cudaError_t e = cudaGetLastError( );
+if( e != cudaSuccess )
+{
+fprintf( stderr, "CUDA failure %s:%d: '%s'\n", __FILE__, __LINE__, cudaGetErrorString(e));}
+}
+
+// array multiplication on the device: C = A * B
+__global__ void ArrayMul( float *dA, float *dB, float *dC )
+{
+int gid = blockIdx.x*blockDim.x + threadIdx.x;
+if( gid < DATASET_SIZE )
+dC[gid] = dA[gid] * dB[gid];
+}
+
+// fill host memory:
+for( int i = 0; i < SIZE; i++ )
+{
+hA[ i ] = hB[ i ] = (float) sqrtf( (float)i );
+}
+// allocate device memory:
+float *dA, *dB, *dC;
+cudaMalloc( (void **)(&dA), sizeof(hA) );
+cudaMalloc( (void **)(&dB), sizeof(hB) );
+cudaMalloc( (void **)(&dC), sizeof(hC) );
+CudaCheckError( );
+
+// copy host memory to the device:
+cudaMemcpy( dA, hA, DATASET_SIZE*sizeof(float), cudaMemcpyHostToDevice );
+cudaMemcpy( dB, hB, DATASET_SIZE*sizeof(float), cudaMemcpyHostToDevice );
+CudaCheckError( )
+
+// setup the execution parameters:
+dim3 grid( DATASET_SIZE / THREADS_PER_BLOCK, 1, 1 );
+dim3 threads( THREADS_PER_BLOCK, 1, 1 );
+// create and start the timer:
+cudaDeviceSynchronize( );
+// allocate the events that we'll use for timing:
+cudaEvent_t start, stop;
+cudaEventCreate( &start );
+cudaEventCreate( &stop );
+CudaCheckError( );
+// record the start event:
+cudaEventRecord( start, NULL );
+CudaCheckError( );
+
+ArrayMul<<< grid, threads >>>( dA, dB, dC );
+
+// record the stop event:
+cudaEventRecord( stop, NULL );
+CudaCheckError( );
+// wait for the stop event to complete:
+cudaEventSynchronize( stop );
+CudaCheckError( );
+float msecTotal;
+cudaEventElapsedTime( &msecTotal, start, stop );
+CudaCheckError( );
+// compute and print the performance
+double secondsTotal = 0.001 * (double)msecTotal;
+double multsPerSecond = (double)DATASET_SIZE / secondsTotal;
+double megaMultsPerSecond = multsPerSecond / 1000000.;
+fprintf( stderr, "%12d\t%4d\t%10.2lf\n", DATASET_SIZE, THREADS_PER_BLOCK, megaMultsPerSecond );
+
+// copy result from the device to the host:
+cudaMemcpy( hC, dC, sizeof(hC), cudaMemcpyDeviceToHost );
+CudaCheckError( );
+// clean up:
+cudaFree( dA );
+cudaFree( dB );
+cudaFree( dC );
+CudaCheckError( );
